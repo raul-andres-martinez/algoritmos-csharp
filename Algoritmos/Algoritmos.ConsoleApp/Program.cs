b1cd7e840c65a55854d2371d63a55c25afe63e84
raul-andres-martinez <rams8@outlook.com>
2023-10-08 15:33:41 -0300

    Exercicio linked list simples

---
+++ Algoritmos/Algoritmos.ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Algoritmos.Domain.Models.LinkedList;
+
+namespace Algoritmos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Faça uma lista encadeada simples, contendo métodos para:
+            //Verificar se existe um valor na lista;
+            //Remover um elemento da lista.
+
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+
+            SimpleLinkedList<int> list = new(new Node<int>(10));
+            list.Add(new Node<int>(20));
+            list.Add(new Node<int>(30));
+            list.Add(new Node<int>(40));
+
+            Console.WriteLine("Lista original:");
+            list.PrintList();
+
+            Node<int> nodeSearch = new(30);
+            var search = list.GetNode(nodeSearch);
+
+            if (search != null)
+            {
+                Console.WriteLine($"Valor {nodeSearch} encontrado na lista.");
+            }
+            else
+            {
+                Console.WriteLine($"Valor {nodeSearch} não encontrado na lista.");
+            }
+
+            Node<int> nodoRemover = new(20);
+            list.Remove(nodoRemover);
+
+            Console.WriteLine("Lista após a remoção:");
+            list.PrintList();
+
+            stopwatch.Stop();
+
+            long time = stopwatch.ElapsedMilliseconds;
+
+            Console.WriteLine($"Tempo de execução: {time} ms");
+        }
+    }
+}
---
+++ Algoritmos/Algoritmos.Domain/Models/LinkedList/Node.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+
+namespace Algoritmos.Domain.Models.LinkedList
+{
+    public class Node<T>
+    {
+        public T? Value { get; set; }
+        public Node<T>? Next { get; set; } = null;
+
+        public Node(T value, Node<T>? next = null)
+        {
+            Value = value; 
+            Next = next;
+        }
+
+        public override string ToString()
+        {
+            return Value?.ToString() ?? "null";
+        }
+    }
+}
